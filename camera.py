d9618d832e49233cd9b74dc2935342b21704ab25
bobsy-narayan <83941340+bobsyn22@users.noreply.github.com>
2024-03-20 19:31:10 -0700

    Updated Camera.py function. Was able to get image from it.
    In future, will need to remove self.save_image from image_callback and test if we can get image using this file as import class

--- camera.py
+++ camera.py
@@ -4,12 +4,10 @@
 #Camera Function Class + camera test
 # Date: 2024-03-08
 
-#Random Empty File. 
-#! /usr/bin/env python
 
 import rospy
 from geometry_msgs.msg import Twist
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CompressedImage
 from sensor_msgs.msg import JointState
 from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import TransformStamped
@@ -21,6 +19,7 @@ import dvrk
 import sys
 from scipy.spatial.transform import Rotation as R
 import os
+import time
 
 class camera:
 
@@ -35,35 +34,44 @@ class camera:
 		self.image_path = os.path.abspath(os.getcwd()) + '/Images/'
 
 
+
 		#full_ros_namespace = self.__ros_namespace + self.__camera_name + '/decklink/camera'
 		full_ros_namespace = self.__ros_namespace + self.__camera_name + '/image_raw'
 
 		#subscriber
-		rospy.Subscriber(full_ros_namespace+ '/image_raw', Image, self.image_callback, queue_size = 1, buff_size = 1000000)
+		rospy.init_node("Camera_call")	
+		rospy.Rate(10000)
+		rospy.Subscriber(full_ros_namespace, Image, self.image_callback, queue_size = 1, buff_size = 1000000)
 
 	def image_callback(self, data):
 
 		try:
 			self.cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+			print('image called')
 		except CvBridgeError as e:
 			print(e)
+			print('failed callback')
+		self.save_image()
 
-	#saves the image in a folder.
-#/dVRK/left/decklink/camera/image_raw
-		#getters
+
 	def get_image(self):
+		print('image receieved')
 		return self.cv_image
-
+		
+	#saves the image in a folder. /home/fizzer/catkin_ws/src/dvrk-ros/dvrk_python/Images
 	def save_image(self):
 
 		if self.cv_image.size != 0:
-			cv2.imwrite(self.image_path + self.__camera_name+"/"+self.__camera_name+"_Camera" +"_" + str(self.image_count)+".png", self.cv_image)
+			#cv2.imwrite(self.image_path + self.__camera_name+"/"+self.__camera_name+"_Camera" +"_" + str(self.image_count)+".png", self.cv_image)
+			cv2.imwrite('/home/fizzer/catkin_ws/src/dvrk-ros/dvrk_python/Images'+str(self.image_count)+'.png',self.cv_image)
 			self.image_count = self.image_count + 1
+			print('image saved')
 			return True
-		else:
+		else:	
+			print('image not saved')
 			return False
+			
 
 if __name__ == '__main__':
 	left_cam=camera('left')
-	left_cam.get_image()
-	left_cam.save_image()
+	rospy.spin()
