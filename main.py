4a9ad3fe00fcd4c851c46cd2d2dca17e42ba05ed
bobsyn22 <83941340+bobsyn22@users.noreply.github.com>
2024-04-09 20:20:38 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -28,6 +28,18 @@ if __name__ == '__main__':
 	# ----- ROS Setup ---------
 	rospy.init_node('mainNode')
 	r = rospy.Rate(100)
+
+	
+	p = dvrk.psm('PSM1') 
+	p.enable()
+	p.home()
+	downLocation = PyKDL.Vector(0,0, 0)
+	goal = p.measured_cp()
+	goal.p = downLocation
+	p.move_cp(goal).wait()
+	currentLocation = p.measured_cp().p
+	
+	
 	TrajctoryMain = trajecTools.TrajctoryNode(homeLocation = (-.1,.05, 0))
 	TrajctoryMain.defualtZLayer = -.17
 	print("Going home")
@@ -35,5 +47,49 @@ if __name__ == '__main__':
 	print("Homed")
 	time.sleep(5)
 
-	TrajctoryMain.pickAndPlace(pickLocation = (-.13,.1), placeLocation = (-.1,-.05))
+	#TrajctoryMain.pickAndPlace(pickLocation = (-.13,.1), placeLocation = (-.1,-.05))
+	TrajctoryMain.pickAndPlace(pickLocation = (-0.13,-0.1), placeLocation = (-.05,-.05))
+
+
+	'''
+	left_cam = camera.camera('left')
+	right_cam = camera.camera('right')
+	ecm = dvrk.arm('ECM')
+
+	#TrajctoryMain.returnHome()
+
+	# -------------------------
+	while isinstance(right_cam.get_image(), list):
+			r.sleep()
+			#print('sleeping')
+	print('cv file recieved')
 	
+
+	#get 2d coordinates from images
+	_, _, coords_2dR, coords_pickupR, _ = imTools.procImage(right_cam.get_image())
+	_, _, coords_2dL, coords_pickupL, _ = imTools.procImage(left_cam.get_image())
+	print("right camera coords",coords_2dR)
+	if not coords_pickupL:
+		coords_pickupL=[0,0]
+	coords_pickupR=coords_pickupL
+	print("right pickup coords",coords_pickupR)
+	print("left pickup coords", coords_pickupL)
+	#get 3d points of starting point
+	
+	coords_3d_pickup = imTools.findDepth(coords_pickupR[0], coords_pickupR[1],
+							coords_pickupL[0], coords_pickupL[1])
+	print('3D pickup coords',coords_3d_pickup)
+	#get 2d coords of end point
+	#ind_to_play=1
+	#print("output coords",coords_2dR)
+	#print("outputcoords[0]",coords_2dR[0])
+	#print("output coords[0][0]]",coords_2dL[0][0])
+	#print("output coords[0][0][0]]",coords_2dL[0][0][0])
+	xr, yr = coords_2dR[0][0]
+	xl, yl = coords_2dL[0][0]
+	print("right dropoff coords",xr,yr)
+	print("left dropoff coords", xl, yl)
+	#get 3d points of end point
+	coords_3d_putdown = imTools.findDepth(xr,yr,xl,yl)
+	print("3D dropoff coords",coords_3d_pickup)
+	'''
